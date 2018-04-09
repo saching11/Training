3a1381b9b0ec5e93333d23fe0fdde4c863ec6d44
saching11 <sachin.gotal@gmail.com>
2018-04-09 21:42:09 +0530

    Max Temperature

---
+++ MaxTemperature/src/com/hadoop/MaxTemperature/MaxTemperatureMapper.java
@@ -0,0 +1,35 @@
+package com.hadoop.MaxTemperature;
+
+import java.io.IOException;
+
+// Hadoop Packages
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+
+public class MaxTemperatureMapper extends Mapper<LongWritable, Text, Text, IntWritable> {
+	
+	public static final int MISSING=9999;
+	
+	public void map( LongWritable key, Text value, Context context )
+		throws IOException, InterruptedException {
+		
+		String line = value.toString();
+		String year = line.substring( 15, 19 );
+		int airTemperature;
+		
+		if( line.charAt( 87 ) == '+' ) {
+			airTemperature = Integer.parseInt( line.substring( 88, 92 ) );
+		} else {
+			airTemperature = Integer.parseInt( line.substring( 87, 92 ) );
+		}
+		
+		String quality = line.substring( 92, 93 );
+		
+		if( airTemperature != MISSING && quality.matches( "[01459]" ) ) {
+			context.write( new Text( year ) ,  new IntWritable( airTemperature ) );
+		}
+	}
+}
