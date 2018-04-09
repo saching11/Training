0237c42cd8943fabf3d437b0dfe90e323438a966
saching11 <sachin.gotal@gmail.com>
2018-04-09 23:09:48 +0530

    Starting word count Example

---
+++ WordCount/src/com/hadoop/WordCount/WordCountMain.java
@@ -0,0 +1,10 @@
+package com.hadoop.WordCount;
+
+public class WordCountMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ WordCount/src/com/hadoop/WordCount/WordCountMapper.java
@@ -0,0 +1,5 @@
+package com.hadoop.WordCount;
+
+public class WordCountMapper {
+
+}
