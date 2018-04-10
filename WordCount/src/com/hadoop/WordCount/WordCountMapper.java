eafab9f2cb775b2b4ca281515fece8a0a61922a8
saching11 <sachin.gotal@gmail.com>
2018-04-10 20:08:14 +0530

    Mapper code for word count

--- WordCount/src/com/hadoop/WordCount/WordCountMapper.java
+++ WordCount/src/com/hadoop/WordCount/WordCountMapper.java
@@ -1,5 +1,20 @@
 package com.hadoop.WordCount;
 
-public class WordCountMapper {
+import java.io.*;
 
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.mapreduce.Mapper;
+
+public class WordCountMapper extends Mapper<LongWritable, Text, Text, IntWritable> {
+
+	public void map( LongWritable key, Text value, Context context ) 
+		throws IOException, InterruptedException{
+		
+		String arrWords[] = value.toString().split( " " );
+		for( String word : arrWords ) {
+			context.write( new Text(word), new IntWritable( 1 ) );
+		}
+	}
 }
