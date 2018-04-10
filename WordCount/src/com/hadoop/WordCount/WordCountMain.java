f910c364351c6facce99a256a7ffa2b2bd340fd4
saching11 <sachin.gotal@gmail.com>
2018-04-10 21:35:09 +0530

    Word count Main class

--- WordCount/src/com/hadoop/WordCount/WordCountMain.java
+++ WordCount/src/com/hadoop/WordCount/WordCountMain.java
@@ -1,10 +1,32 @@
 package com.hadoop.WordCount;
 
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.conf.*;
+import org.apache.hadoop.mapreduce.Job;
+
+import org.apache.hadoop.fs.*;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
 public class WordCountMain {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		// TODO Auto-generated method stub
-
+		Configuration config = new Configuration();
+		Job job = new Job( config, "Word Count" );
+		
+		job.setJarByClass( WordCountMain.class );
+		job.setMapperClass( WordCountMapper.class );
+		job.setReducerClass( WordCountReducer.class );
+		
+		job.setOutputKeyClass( Text.class );
+		job.setOutputValueClass( IntWritable.class );
+		
+		FileInputFormat.addInputPath( job, new Path( args[0] ) );
+		FileOutputFormat.setOutputPath( job, new Path( args[1] ) );
+		
+		System.exit( job.waitForCompletion( true ) ? 0 : 1 );
 	}
 
 }
