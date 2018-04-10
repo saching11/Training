10322157a6fb64479bd8e466c0976332e1fc1a6e
saching11 <sachin.gotal@gmail.com>
2018-04-10 20:24:08 +0530

    Reducer Program

--- WordCount/src/com/hadoop/WordCount/WordCountReducer.java
+++ WordCount/src/com/hadoop/WordCount/WordCountReducer.java
@@ -1,5 +1,21 @@
 package com.hadoop.WordCount;
 
-public class WordCountReducer {
+import java.io.*;
 
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.mapreduce.Reducer;
+
+public class WordCountReducer extends Reducer< Text, IntWritable, Text, IntWritable > {
+
+	public void reduce( Text key, Iterable<IntWritable> values, Context context )
+		throws IOException, InterruptedException{
+		
+		int count = 0;
+		for( IntWritable value : values ) {
+			count = count + value.get();
+		}
+		
+		context.write( key, new IntWritable( count ) );
+	}
 }
